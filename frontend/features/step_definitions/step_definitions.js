7e94da35596e09dce311165c27a8fc01f52d9d2b
saagar14 <saagar14@tamu.edu>
2017-10-09 11:20:40 -0500

    Frontend Test Feature Update

--- frontend/features/step_definitions/step_definitions.js
+++ frontend/features/step_definitions/step_definitions.js
@@ -4,24 +4,51 @@ var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 var expect = chai.expect;
 module.exports = function() {
-    
-    this.Given(/^I go to "([^"]*)"$/, function(site) {
-        browser.get(site);
+
+      //LOGIN VALID CREDENTIALS
+      this.Given(/^I am on the Login Page$/, function(site) {
+        // browser.get(site);
       });
     
-      this.When(/^I add "([^"]*)" in the task field$/, function(task) {
-        element(by.model('todoList.todoText')).sendKeys(task);
+      this.When(/^I enter valid username$/, function(task) {
+        // element(by.model('todoList.todoText')).sendKeys(task);
       });
     
-      this.When(/^I click the add button$/, function() {
-        var el = element(by.css('[value="add"]'));
-        el.click();
+      this.When(/^I enter associated password$/, function(task) {
+        // element(by.model('todoList.todoText')).sendKeys(task);
+
+      });
+
+      this.When(/^I click the Login button$/, function() {
+        // var el = element(by.css('[value="add"]'));
+        // el.click();
       });
     
-      this.Then(/^I should see my new task in the list$/, function(callback) {
-        var todoList = element.all(by.repeater('todo in todoList.todos'));
-        expect(todoList.count()).to.eventually.equal(3);
-        expect(todoList.get(2).getText()).to.eventually.equal('Do not Be Awesome')
-          .and.notify(callback);
+      this.Then(/^I should be able to enter the Portal Dashboard$/, function(callback) {
+        // var todoList = element.all(by.repeater('todo in todoList.todos'));
+        // expect(todoList.count()).to.eventually.equal(3);
+        // expect(todoList.get(2).getText()).to.eventually.equal('Do not Be Awesome')
+        //   .and.notify(callback);
       });
+
+      //LOGIN INVALID CREDENTIALS
+    
+      this.When(/^I enter invalid credentials$/, function(task) {
+        // element(by.model('todoList.todoText')).sendKeys(task);
+          // element(by.model('todoList.todoText')).sendKeys(task);
+      });
+    
+      this.Then(/^I should receive a message that my "([^"]*)"$/, function(callback) {
+        // var todoList = element.all(by.repeater('todo in todoList.todos'));
+        // expect(todoList.count()).to.eventually.equal(3);
+        // expect(todoList.get(2).getText()).to.eventually.equal('Do not Be Awesome')
+        //   .and.notify(callback);
+      });
+      this.Then(/^I should receive a message that my "([^"]*)"$/, function(callback) {
+        // var todoList = element.all(by.repeater('todo in todoList.todos'));
+        // expect(todoList.count()).to.eventually.equal(3);
+        // expect(todoList.get(2).getText()).to.eventually.equal('Do not Be Awesome')
+        //   .and.notify(callback);
+      });
+      
     }
