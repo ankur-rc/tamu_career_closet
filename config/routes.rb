09dfa6c6d5e5e3338f5555b48b52b6912336129e
saagar14 <saagar14@tamu.edu>
2017-10-09 10:35:38 -0500

    Angular Integration Test

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :rentals
   resources :apparels
   resources :students
+  #root 'angular#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
