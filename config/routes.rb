12926453720655802bdd21d980e8d15bac0661a7
saagar14 <saagar14@tamu.edu>
2017-11-04 16:54:47 -0500

    API Versioning

--- config/routes.rb
+++ config/routes.rb
@@ -2,33 +2,52 @@ Rails.application.routes.draw do
 
 
 
-  match '/rentals/viewActive' => 'rentals#view_active_user', via: :get
-  match '/rentals/viewCheckedOut' => 'rentals#view_checkedOut', via: :get
-  match '/rentals/activeUsersAndCheckedOutApparels' => 'rentals#num_active_users_and_checked_out', via: :get
-  match '/apparels/getSizes' => 'apparels#get_sizes', via: :get
-  match '/students/new' => 'students#create', via: :post
-  match '/students/delete' => 'students#destroy', via: :get
-  match '/students/show' => 'students#show', via: :get
-  match '/students/update' => 'students#update', via: :post
+  # match '/rentals/viewActive' => 'rentals#view_active_user', via: :get
+  # match '/rentals/viewCheckedOut' => 'rentals#view_checkedOut', via: :get
+  # match '/rentals/activeUsersAndCheckedOutApparels' => 'rentals#num_active_users_and_checked_out', via: :get
+  # match '/apparels/getSizes' => 'apparels#get_sizes', via: :get
+  # match '/students/new' => 'students#create', via: :post
+  # match '/students/delete' => 'students#destroy', via: :get
+  # match '/students/show' => 'students#show', via: :get
+  # match '/students/update' => 'students#update', via: :post
 
-
-
-
-
-
-  get "rentals/pending_returnsAndDefaulters", to: "rentals#pending_returnsAndDefaulters", as: "pendingreturns",:defaults => { :format => 'json' }
-  get "rentals/assignsuits/:studentUIN/:apparelId", to: "rentals#assignSuits",as: "assignsuits",:default=>{:format=>'json'}
-  resources :constants
-
-  resources :rentals
-
-  resources :apparels
-  resources :students
+  # get "rentals/pending_returnsAndDefaulters", to: "rentals#pending_returnsAndDefaulters", as: "pendingreturns",:defaults => { :format => 'json' }
+  # get "rentals/assignsuits/:studentUIN/:apparelId", to: "rentals#assignSuits",as: "assignsuits",:default=>{:format=>'json'}
+  # resources :constants
+  # resources :rentals
+  # resources :apparels
+  # resources :students
 
   root 'students#home'
   # root 'rentals#index'
+  # namespace the controllers without affecting the URI
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :constants
+    resources :rentals
+    resources :apparels
+    resources :students 
+  end
+  
+  #Login and Registration
+
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
+  
+  #Rentals  
+  get 'rentals', to: 'rentals#index'
+  # get 'rentals/(:id)', to: 'rentals#new'
+  get 'rentals/activeusers', to: 'rentals#view_active_user'
+  get 'rentals/checkedout', to: 'rentals#view_checkedOut'
+  get 'rentals/checkedactive', to: 'rentals#num_active_users_and_checked_out'
+  # post 'rentals/(:id)'
+
+  #Students
+  get 'students', to: 'students#index'
+
+  
+  #Apparels
+  get 'apparels', to: 'apparels#index'
+  
 
 
 
