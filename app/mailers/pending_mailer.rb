18bb1389876b7d9bfe9ff5bfd114d3f591b28fc7
Ashok Govinda Gowda <8asokagowda@gmail.com>
2017-11-06 00:19:29 -0600

    Commits for spacing

--- app/mailers/pending_mailer.rb
+++ app/mailers/pending_mailer.rb
@@ -4,10 +4,10 @@ class PendingMailer < ApplicationMailer
   def sendPendingMail(student_uin,student_name,student_email,checkout_date,expected_return_date)
     staticTextForPendingMail=Constant.findValue("pendingMailText")
     pendingMailSenderName=Constant.findValue("pendingMailSenderName")
-    text=" Hi" +student_name+","+"\n"
-    text+=" UIN :"+student_uin.to_s+"\n"
-    text+=" Checkout Date of Suit: "+Time.parse(checkout_date.to_s).strftime("%m/%d/%Y")+"\n"
-    text+=" Expected Return Date Of Suit: "+ Time.parse(expected_return_date.to_s).strftime("%m/%d/%Y")+"\n"
+    text="Hi " +student_name+","+"\n\n"
+    text+="UIN :"+student_uin.to_s+"\n"
+    text+="Checkout Date of Suit: "+Time.parse(checkout_date.to_s).strftime("%m/%d/%Y")+"\n"
+    text+="Expected Return Date Of Suit: "+ Time.parse(expected_return_date.to_s).strftime("%m/%d/%Y")+"\n"
     text+=staticTextForPendingMail
     text+="\n"+"Regards,"
     text+="\n"+pendingMailSenderName
@@ -18,10 +18,10 @@ class PendingMailer < ApplicationMailer
   def sendOverDueEmails(student_uin,student_name,student_email,checkout_date,expected_return_date)
     staticTextForOverDueMail=Constant.findValue("overDueMailText")
     overDueMailSenderName=Constant.findValue("overDueMailSenderName")
-    text=" Hi" +student_name+","+"\n"
-    text+=" UIN :"+student_uin.to_s+"\n"
-    text+=" Checkout Date of Suit: "+Time.parse(checkout_date.to_s).strftime("%m/%d/%Y")+"\n"
-    text+=" Expected Return Date Of Suit: "+Time.parse( expected_return_date.to_s).strftime("%m/%d/%Y")+"\n"
+    text="Hi " +student_name+","+"\n"
+    text+="UIN :"+student_uin.to_s+"\n"
+    text+="Checkout Date of Suit: "+Time.parse(checkout_date.to_s).strftime("%m/%d/%Y")+"\n"
+    text+="Expected Return Date Of Suit: "+Time.parse( expected_return_date.to_s).strftime("%m/%d/%Y")+"\n"
     text+=staticTextForOverDueMail
     text+="\n"+"Regards,"
     text+="\n"+overDueMailSenderName
