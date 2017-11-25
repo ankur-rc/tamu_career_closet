d613b3f95bc54f1f90900a88808ac15cc692f47e
saagar14 <saagar14@tamu.edu>
2017-11-25 05:27:32 -0600

    Route change fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # namespace the controllers without affecting the URI
+  # Login and Registration API
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
+  root 'rentals#index'
+
   scope module: :v1, constraints: ApiVersion.new('v1', true) do
-    root 'rentals#index'
-
-    # Login and Registration API
-    post 'auth/login', to: 'authentication#authenticate'
-    post 'signup', to: 'users#create'
-
+    
     # Rentals API
     get 'rentals', to: 'rentals#index'
     get 'rentals/activeusers', to: 'rentals#view_active_user'
@@ -14,13 +14,18 @@ Rails.application.routes.draw do
     get 'rentals/checkedactive', to: 'rentals#num_active_users_and_checked_out'
     get 'rentals/returns', to: 'rentals#pending_returns'
     get 'rentals/defaulters', to: 'rentals#pending_returns_and_defaulters'
-    get 'renatls/sendpendingemails', to: 'rentals#send_pending_emails'
-    get 'renatls/sendoverdueemails',  to: 'rentals#send_overdue_emails'
+    ####################
+    get 'rentals/sendpendingemails', to: 'rentals#send_pending_emails'
+    get 'rentals/sendoverdueemails',  to: 'rentals#send_overdue_emails'
     get 'rentals/listreports', to: 'rentals#list_reports'
     get 'rentals/newreport', to: 'rentals#new_report', defaults: { format: :csv }
+    get 'rentals/studentbyrental/:apparel_id', to: 'rentals#getstudent'
+    ####################
     get 'rentals(/:id)', to: 'rentals#show'
+    ####################
     post 'rentals/assign', to: 'rentals#assign_suit'
     post 'rentals/return', to: 'rentals#return_suit'
+    ####################
     post 'rentals', to: 'rentals#create'
     put 'rentals/:id', to: 'rentals#update'
     delete 'rentals/:id', to: 'rentals#destroy'
@@ -34,11 +39,13 @@ Rails.application.routes.draw do
     
     # Apparels API
     get 'apparels', to: 'apparels#index'
+    ###################
     get 'apparels/bysize/:size', to: 'apparels#bysize'
     get 'apparels/bysizeandstock/:size/:stock', to: 'apparels#bysize_and_stock'
     get 'apparels/getcheckedoutstock', to: 'apparels#get_stock', :defaults => { :stock => 1 }
     get 'apparels/getavailablestock', to: 'apparels#get_stock', :defaults => { :stock => 0 }
     get 'apparels/getsizes', to: 'apparels#get_sizes'
+    ###################
     get 'apparels(/:id)', to: 'apparels#show'
     post 'apparels', to: 'apparels#create'
     put  'apparels/:id', to: 'apparels#update'
@@ -47,5 +54,10 @@ Rails.application.routes.draw do
     #Constants API
     get 'constants/getconstants', to: 'constants#show_constants'
     post 'constants/updateconstants', to: 'constants#update_constant'
+
+    resources :constants
+    resources :rentals
+    resources :apparels
+    resources :students
   end
 end
